dfc313d3936b0df27f58163fd9decc95076c4caa
Arif Hossain <devarifhossain@gmail.com>
2021-12-29 11:34:43 +0600

    added discord bot

---
+++ bot/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import Discord from "discord.js"; //import discord.js
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Discord.Client({
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+});
+
+const app = express();
+app.use(express.json());
+
+const discord = () => {
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+
+  client.login(process.env.BOT_TOKEN);
+};
+discord();
+
+app.get("/", (req, res) => {
+  console.log("res", res);
+
+  res.send("HI from arif");
+});
+
+app.post("/", async (req, res) => {
+  const { email, name, message } = req.body;
+
+  const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
+  channel.send(
+    `${name} send a new message through sellify contact from. \nemail: ${email} \nmessage: ${message} \n------------`
+  );
+
+  res.send("this is a post request");
+});
+
+app.listen(5000, () => {
+  console.log("the app is running on port 5000");
+});
